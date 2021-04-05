31af15ce36bf60e225f71d76cb9008d5e451eef6
DAM-1 <DAM-1@PC24>
2021-04-05 09:52:41 +0200

    ya

---
+++ historialWeb/src/historialWeb/EntradaHistorial.java
@@ -0,0 +1,11 @@
+package historialWeb;
+
+import java.time.LocalDateTime;
+
+public class EntradaHistorial {
+
+	private String url;
+	private LocalDateTime fechaHora;
+	
+	
+}
---
+++ historialWeb/src/historialWeb/Historial.java
@@ -0,0 +1,13 @@
+package historialWeb;
+
+import java.util.ArrayList;
+
+public class Historial {
+
+	private ArrayList<EntradaHistorial> listHistorial;
+
+	public Historial() {
+		listHistorial = new ArrayList<EntradaHistorial>();
+	}
+
+}
